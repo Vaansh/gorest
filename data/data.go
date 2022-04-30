4c4696edc3a6027919ab3fa4714aebe8f592caa9
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2022-04-30 01:35:25 -0400

    add data

--- data/data.go
+++ data/data.go
@@ -1,1 +1,26 @@
 package data
+
+import (
+	"github.com/google/uuid"
+)
+
+type item struct {
+	ID    string  `json:"id"`
+	Name  string  `json:"name"`
+	Price float64 `json:"price"`
+}
+
+func GenerateID() string {
+	id := uuid.New()
+	return id.String()
+}
+
+var items = []item{
+	{ID: GenerateID(), Name: "Item1", Price: 14.99},
+	{ID: GenerateID(), Name: "Item2", Price: 24.00},
+	{ID: GenerateID(), Name: "Item3", Price: 5.49},
+}
+
+func GetItemsRaw() []item {
+	return items
+}
--- main.go
+++ main.go
@@ -1,1 +1,5 @@
-package gorest
+package main
+
+func main() {
+
+}
